e2b7c5da52e938fb0a2e6974cc530fca9a873363
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-06-30 20:01:20 +0530

    .

---
+++ lib/bottom.dart
@@ -0,0 +1,161 @@
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+
+
+
+class MyCustomBottomNavigationBar extends StatefulWidget {
+  @override
+  MyCustomBottomNavigationBarState createState() =>
+      MyCustomBottomNavigationBarState();
+}
+
+class MyCustomBottomNavigationBarState
+    extends State<MyCustomBottomNavigationBar> {
+  var currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(brightness: Brightness.dark),
+      body: null,
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.all(_width * .05),
+        height: _width * .155,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.1),
+              blurRadius: 30,
+              offset: Offset(0, 10),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: ListView.builder(
+          itemCount: 4,
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.symmetric(horizontal: _width * .02),
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {
+              setState(() {
+                currentIndex = index;
+                HapticFeedback.lightImpact();
+              });
+            },
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Stack(
+              children: [
+                AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  width: index == currentIndex ? _width * .32 : _width * .18,
+                  alignment: Alignment.center,
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    height: index == currentIndex ? _width * .12 : 0,
+                    width: index == currentIndex ? _width * .32 : 0,
+                    decoration: BoxDecoration(
+                      color: index == currentIndex
+                          ? Colors.blueAccent.withOpacity(.2)
+                          : Colors.transparent,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+                AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  width: index == currentIndex ? _width * .31 : _width * .18,
+                  alignment: Alignment.center,
+                  child: Stack(
+                    children: [
+                      Row(
+                        children: [
+                          AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            width: index == currentIndex ? _width * .13 : 0,
+                          ),
+                          AnimatedOpacity(
+                            opacity: index == currentIndex ? 1 : 0,
+                            duration: Duration(seconds: 1),
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            child: Text(
+                              index == currentIndex
+                                  ? '${listOfTitles[index]}'
+                                  : '',
+                              style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            width: index == currentIndex ? _width * .03 : 20,
+                          ),
+                          Icon(
+                            listOfIcons[index],
+                            size: _width * .076,
+                            color: index == currentIndex
+                                ? Colors.blueAccent
+                                : Colors.black26,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<IconData> listOfIcons = [
+    Icons.home_rounded,
+    Icons.favorite_rounded,
+    Icons.settings_rounded,
+    Icons.person_rounded,
+  ];
+
+  List<String> listOfTitles = [
+    'Home',
+    'Favorite',
+    'Settings',
+    'Account',
+  ];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
